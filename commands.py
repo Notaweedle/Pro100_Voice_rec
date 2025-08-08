4f05690fac732f34bba57c1776b054f3a3bd628c
jkwiatkowski <jkwiatkowski@student.neumont.edu>
2025-08-08 16:12:20 -0600

    commands update

--- commands.py
+++ commands.py
@@ -7,20 +7,16 @@ def commands(command):
 
     if "exit" in command:
         os._exit(0)
-
-    if 'steam' in command:
+    elif 'steam' in command:
         ## logic can be applied to open any .exe
         key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Valve\Steam")
         path, _ = winreg.QueryValueEx(key, "SteamExe")
         winreg.CloseKey(key)
         subprocess.Popen([path])
-
-    if 'call of duty' in command:
+    elif 'call of duty' in command:
         os.startfile(r"put_path_here")
-
-    if 'open browser' in command:
+    elif 'open browser' in command:
         ##web-browser open to url
         webbrowser.open_new('www.google.com')
-
     else:
         print("No command found")
